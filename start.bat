6a3c3141f356aa05dcafa666aa618ec679a754ee
Alex <57352352+Aliak-Roi@users.noreply.github.com>
2022-04-22 20:23:18 +0100

    Delete start.bat

--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/Aliak-Roi/spotify-patch/master/install.ps1' | Invoke-Expression}"
-pause
-exit
